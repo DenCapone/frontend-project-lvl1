5ccc96a38e255c03b6ecb1b2a6bf72cd19ad0b90
Den Capone <Afrosin2012@gmail.com>
2020-02-18 00:43:13 +0300

    Game beta version

--- src/index.js
+++ src/index.js
@@ -8,4 +8,36 @@ const greeting = () => {
   return console.log(`Hello ${userName}!`);
 };
 
-export  {greeting};
+const startGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let score = 0;
+
+  for (score; score < 3; score += 1) {
+    const randomNumber = Math.ceil(Math.random() * 100);
+    console.log(`Question: ${randomNumber}`);
+    if (randomNumber % 2 === 0) {
+      const answerForEvenNumber = readlineSync.question('Your answer: ');
+      if (answerForEvenNumber !== 'yes') {
+        console.log(`"${answerForEvenNumber}" is wrong answer ;(. Correct answer was "yes".`);
+        break;
+      } else {
+        console.log('Correct!');
+      }
+    }
+    if (randomNumber % 2 !== 0) {
+      const answerForNotEvenNumber = readlineSync.question('Your answer: ');
+      if (answerForNotEvenNumber !== 'no') {
+        console.log(`"${answerForNotEvenNumber}" is wrong answer ;(. Correct answer was "no".`);
+        break;
+      } else {
+        console.log('Correct!');
+      }
+    }
+  }
+  return score === 3 ? console.log(`Congratulations, ${userName}!`) : console.log(`Let's try again, ${userName}!`);
+};
+
+export { greeting, startGame };
